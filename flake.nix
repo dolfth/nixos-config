017c2efeacb35002234a17a59b123be39e6387a2
Dolf ter Hofste <dolf@example.com>
2025-01-08 13:58:19 +0100

    add My First Flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+# /etc/nixos/flake.nix
+{
+  description = "flake for nwa";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      nwa = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
